65d5c001651eb6d9df71f0c9e1e37638481b3742
Kevin Bond <kevinbond@gmail.com>
2022-09-30 16:04:18 -0400

    [feature] add `UriLink` & `UriLinks` (#13)

---
+++ src/Uri/Link/UriLinks.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Zenstruck\Uri\Link;
+
+use Psr\Link\LinkInterface;
+use Symfony\Component\WebLink\GenericLinkProvider;
+use Symfony\Component\WebLink\HttpHeaderSerializer;
+use Traversable;
+use Zenstruck\Uri\Stringable;
+
+if (!\class_exists(GenericLinkProvider::class)) {
+    throw new \LogicException('symfony/web-link (4.4+) is required to use this class. Install with "composer require symfony/web-link".');
+}
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ *
+ * @implements \IteratorAggregate<UriLink>
+ */
+final class UriLinks extends GenericLinkProvider implements \Stringable, \Countable, \IteratorAggregate
+{
+    use Stringable;
+
+    public static function fromString(string $serialized): self
+    {
+        return new self(\array_map([UriLink::class, 'fromString'], \explode(',', $serialized)));
+    }
+
+    public function firstForRel(string $rel): ?UriLink
+    {
+        return $this->getLinksByRel($rel)[0] ?? null;
+    }
+
+    /**
+     * @return UriLink[]
+     */
+    public function getLinks(): array
+    {
+        return self::normalizeLinks(parent::getLinks());
+    }
+
+    /**
+     * @param string $rel
+     *
+     * @return UriLink[]
+     */
+    public function getLinksByRel($rel): array
+    {
+        return self::normalizeLinks(parent::getLinksByRel($rel));
+    }
+
+    public function count(): int
+    {
+        return \count($this->getLinks());
+    }
+
+    public function getIterator(): Traversable
+    {
+        return new \ArrayIterator($this->getLinks());
+    }
+
+    protected function generateString(): string
+    {
+        return (string) (new HttpHeaderSerializer())->serialize($this->getLinks()); // @phpstan-ignore-line
+    }
+
+    /**
+     * @param LinkInterface[] $links
+     *
+     * @return UriLink[]
+     */
+    private static function normalizeLinks(array $links): array
+    {
+        return \array_map([UriLink::class, 'wrap'], $links);
+    }
+}
---
+++ tests/Link/UriLinksTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Zenstruck\Uri\Tests\Link;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\WebLink\Link;
+use Zenstruck\Uri;
+use Zenstruck\Uri\Link\UriLink;
+use Zenstruck\Uri\Link\UriLinks;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+final class UriLinksTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function to_string(): void
+    {
+        $this->assertSame(
+            '<https://foo.com>; rel="",<https://bar.com>; rel="next",<https://baz.com>; rel="next prev"; foo="bar",<https://qux.com>; rel="next"; baz="1"',
+            (string) $this->sut(),
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function from_string(): void
+    {
+        $links = UriLinks::fromString('<https://foo.com>; rel="",<https://bar.com>; rel="next",<https://baz.com>; rel="next prev"; foo="bar",<https://qux.com>; rel="next"');
+
+        $this->assertSame(
+            [
+                'https://foo.com',
+                'https://bar.com',
+                'https://baz.com',
+                'https://qux.com',
+            ],
+            \array_map(fn(UriLink $link) => $link->uri()->toString(), $links->getLinks())
+        );
+
+        $this->assertSame(['next'], $links->getLinks()[1]->getRels());
+        $this->assertSame([], $links->getLinks()[1]->getAttributes());
+        $this->assertSame(['next', 'prev'], $links->getLinks()[2]->getRels());
+        $this->assertSame(['foo' => 'bar'], $links->getLinks()[2]->getAttributes());
+    }
+
+    /**
+     * @test
+     */
+    public function countable_and_iterable(): void
+    {
+        $links = $this->sut();
+
+        $this->assertCount(4, $links);
+        $this->assertSame(
+            [
+                'https://foo.com',
+                'https://bar.com',
+                'https://baz.com',
+                'https://qux.com',
+            ],
+            \array_map(fn(UriLink $link) => $link->uri()->toString(), \iterator_to_array($links))
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function links_are_normalized(): void
+    {
+        $links = $this->sut();
+
+        $this->assertSame(
+            [
+                'https://foo.com',
+                'https://bar.com',
+                'https://baz.com',
+                'https://qux.com',
+            ],
+            \array_map(fn(UriLink $link) => $link->uri()->toString(), $links->getLinks())
+        );
+        $this->assertSame(
+            [
+                'https://bar.com',
+                'https://baz.com',
+                'https://qux.com',
+            ],
+            \array_map(fn(UriLink $link) => $link->uri()->toString(), $links->getLinksByRel('next'))
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function first_for_rel(): void
+    {
+        $links = $this->sut();
+
+        $this->assertNull($links->firstForRel('invalid'));
+        $this->assertSame('https://bar.com', $links->firstForRel('next')->uri()->toString());
+        $this->assertSame('https://baz.com', $links->firstForRel('prev')->uri()->toString());
+    }
+
+    private function sut(): UriLinks
+    {
+        return new UriLinks([
+            new UriLink('https://foo.com'),
+            new UriLink(new Uri('https://bar.com'), 'next'),
+            (new UriLink('https://baz.com', 'next'))->withRel('prev')->withAttribute('foo', 'bar'),
+            (new Link('next', 'https://qux.com'))->withAttribute('baz', 1),
+        ]);
+    }
+}
